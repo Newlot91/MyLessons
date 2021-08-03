11085e903e1ddd6323b897b2f4087556b5780c46
Newlot91 <erm.alexander.1991@gmail.com>
2021-08-04 00:04:06 +0300

    Added 2 uni tests (GetNumberOfNumbersInString,GetMaxWordTest)

--- Day_9/MyLibTests/MyLiblaryTests.cs
+++ Day_9/MyLibTests/MyLiblaryTests.cs
@@ -14,7 +14,25 @@ namespace MyLib.Tests
         [TestMethod()]
         public void GetNumberOfNumbersInStringTest()
         {
-            Assert.AreEqual
+            //arrange
+            string s = "da57zxv7v8";
+            int expected = 4;
+            //act
+            int actual =MyLiblary.GetNumberOfNumbersInString(s);
+            //assert
+            Assert.AreEqual(expected,actual);
+
+        }
+        [TestMethod()]
+        public void GetMaxWordTest()
+        {
+            //arrange
+            string[] sArray = { "hi", "Albert", "Sam", "FiberGlass", "Leam" };
+            string expected = "FiberGlass";
+            //act
+            string actual = MyLiblary.GetMaxWord(sArray);
+            //assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
