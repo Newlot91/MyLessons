075b66cb77a4af2a5fdf9c1122be9863ce69a464
Newlot91 <erm.alexander.1991@gmail.com>
2021-11-09 20:44:53 +0300

    Added methods;

---
+++ Try-catch/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                NullRefExepcion(null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                Console.WriteLine($"Method: {ex.TargetSite}");
+                Console.WriteLine($"Stack trace: {ex.StackTrace}");
+                Console.WriteLine();
+            }
+
+            try
+            {
+                int[] nums=new int [2];
+                IndexOutOfRange(nums, 5);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                Console.WriteLine($"Method: {ex.TargetSite}");
+                Console.WriteLine($"Stack trace: {ex.StackTrace}");
+                Console.WriteLine();
+            }
+
+            try
+            {
+                Object obj = "one";
+                int aa = (Int32)obj;
+            }
+            catch (InvalidCastException ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                Console.WriteLine($"Method: {ex.TargetSite}");
+                Console.WriteLine($"Stack trace: {ex.StackTrace}");
+                Console.WriteLine();
+            }
+            //With throw
+            void NullRefExepcion(string line)
+            {
+                if (line == null)
+                {
+                    throw new Exception("Null reference");
+                }
+                else
+                {
+                    Console.WriteLine("Value is not NULL");
+                }
+            }            
+            void IndexOutOfRange(int []nums,int indexOfNum )
+            {
+                if (indexOfNum> nums.Length)
+                {
+                    throw new Exception("Index out of the range");
+                }
+                else
+                {
+                    Console.WriteLine($"Array lenght is {nums.Length}\nSelected index is {indexOfNum}");
+                }
+            }
+            void CastExcepcion()//не знаю как выполнить проверку
+            {
+
+            }
+        }
+    }
+}
